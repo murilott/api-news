55ca1d2990d777f9d540d7e11e389cdb60a2ac31
murilott <murilo_teichert@hotmail.com>
2025-04-04 11:46:12 -0300

    rota soma

--- index.js
+++ index.js
@@ -2,8 +2,23 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
+// Método HTTP - CRUD
+    // GET - pega uma info
+    // POST - cria uma info
+    // PUT - altera toda a info
+    // PATH - altera parte da info
+    // DELETE - apaga uma info
+// Nome - um identificador da rota
+// Função (Callback) - responsável por executar algum comando
+
+app.get('/soma', (req, res) => {
+    const soma = 2 + 2;
+
+  res.send({soma: soma})
+})
+
+app.get('/home', (req, res) => {
+  res.send('Bem vindo')
 })
 
 app.listen(3000)
